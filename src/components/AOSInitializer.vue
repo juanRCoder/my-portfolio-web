46720da16d87c6b04b9eaf69c28fa8bebbd5b368
juanRCoder <guillecarck@gmail.com>
2026-03-06 22:13:18 -0500

    refactor: finish portfolio 2026

---
+++ src/components/AOSInitializer.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { onMounted } from "vue";
+import "aos/dist/aos.css";
+import AOS from "aos";
+
+onMounted(() => {
+  AOS.init();
+});
+</script>
+
+<template>
+  <span style="display: none" />
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav class="bg-[#5F5E5E]/5 backdrop-blur-lg sticky top-0 text-neutral-300">
+    <ul class="flex justify-center items-center gap-5 py-4">
+      <a href="#skills" class="">Habilidades</a>
+      <a href="#experiences" class="">Experiencias</a>
+      <a href="#contact" class="">Contacto</a>
+    </ul>
+  </nav>
+</template>
+<script lang="ts"></script>
+<style lang="css"></style>
